99584db781c957d6e61fea413fe7744bf4a23bc4
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-08-16 23:49:45 +0530

    Update 03_Search in a 2 D matrix-2(Optimal).cpp

--- Step4/Lec3/BS on 2D Arrays/03_Search in a 2 D matrix-2(Optimal).cpp
+++ Step4/Lec3/BS on 2D Arrays/03_Search in a 2 D matrix-2(Optimal).cpp
@@ -1,1 +1,13 @@
+bool searchElement(vector<vector<int>> &MATRIX, int target) {
+    int n = MATRIX.size();
+    int m = MATRIX[0].size();
+    int row = 0;
+    int col = m-1;
 
+    while(row<n && col >= 0){
+        if(MATRIX[row][col] == target) return true;
+        else if(MATRIX[row][col] < target) row++;
+        else col--;
+    }
+    return false;
+}
