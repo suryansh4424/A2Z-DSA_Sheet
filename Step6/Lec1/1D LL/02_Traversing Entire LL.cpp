26c4fc8bced4737d690b21a7750787c78564adcf
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-08-19 19:41:34 +0530

    Update 02_Traversing Entire LL.cpp

--- Step6/Lec1/1D LL/02_Traversing Entire LL.cpp
+++ Step6/Lec1/1D LL/02_Traversing Entire LL.cpp
@@ -1,16 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Node {
     public:
-        int data;         // Data stored in the node
-        Node* next;       // Pointer to the next node in the linked list
-        // Constructors
+        int data;
+        Node* next;
+
         Node(int data1, Node* next1) {
             data = data1;
             next = next1;
         }
-        // Constructor
+
         Node(int data1) {
             data = data1;
             next = nullptr;
         }
 };
 
+Node* constructLL(vector<int> &arr) {
+    if (arr.empty()) return nullptr;
+
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+
+    for (int i = 1; i < arr.size(); i++) {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+int main() {
+    vector<int> arr = {2, 5, 8, 7};
+    Node* head = constructLL(arr);
+    Node* temp = head;
+
+    while (temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
