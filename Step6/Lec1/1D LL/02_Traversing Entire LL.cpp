3ffb1a9045480e011f1d90f4a8e9b9f6d39e796c
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-08-19 19:37:11 +0530

    Create 02_Traversing Entire LL.cpp

---
+++ Step6/Lec1/1D LL/02_Traversing Entire LL.cpp
@@ -0,0 +1,16 @@
+class Node {
+    public:
+        int data;         // Data stored in the node
+        Node* next;       // Pointer to the next node in the linked list
+        // Constructors
+        Node(int data1, Node* next1) {
+            data = data1;
+            next = next1;
+        }
+        // Constructor
+        Node(int data1) {
+            data = data1;
+            next = nullptr;
+        }
+};
+
