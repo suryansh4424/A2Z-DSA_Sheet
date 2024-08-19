ee685060e28fc2d3e340cdccb98bdb48195d2348
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-08-19 15:34:38 +0530

    Create 02_Majority Element (Better).cpp

---
+++ Step3/Lec3/Array's Hard/02_Majority Element (Better).cpp
@@ -0,0 +1,14 @@
+vector<int> majorityElement(vector<int> v) {
+    int n = v.size();
+    vector<int> ls;
+    map<int,int> mpp;
+    int mini = int(n/3) + 1;
+    for(int i=0; i<n; i++){
+        mpp[v[i]]++;
+        if(mpp[v[i]] == mini){
+            ls.push_back(v[i]);
+        }
+        if(ls.size() == 2)break;
+    }
+    return ls;
+}
