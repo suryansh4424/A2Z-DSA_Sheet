c77c099ce64faeeb51ea114e84c5aba206505d44
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-08-20 12:27:34 +0530

    Create 08_Deleting element in LL.cpp

---
+++ Step6/Lec1/1D LL/08_Deleting element in LL.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* constructLL(vector<int> &arr) {
+    if (arr.empty()) return nullptr;
+
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+
+    for (int i = 1; i < arr.size(); i++) {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+void printLL(Node* head) {
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+//CODE FOR HEAD DELETION
+Node* removek(Node* head, int element){
+    if(head == NULL) return head;
+    if(head->data == element){
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp != NULL){
+        if(temp->data == element){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+int main() {
+    vector<int> arr = {12, 5, 8, 7};
+    Node* head = constructLL(arr);
+    head = removek(head, 5);
+    printLL(head);
+
+    return 0;
+}
