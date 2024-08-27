c087a6945e697fed8da1402a16a9b6d0d4ab6f6b
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-08-27 23:24:56 +0530

    Create 03_Insertion_Sort.cpp

---
+++ Step2/Lec1/Sorting-1/03_Insertion_Sort.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(int arr[], int n){
+    for(int i=0; i<=n-1; i++){
+        int j = i;
+        while(j>0 && arr[j-1] > arr[j]){
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j --;
+        }
+        
+    }
+}
+
+int main(){
+    int n;
+    cout << "Number= ";
+    cin >> n;
+    int arr[n];
+    cout << "Array of size " << n <<" :";
+    for(int i=0; i<n; i++) cin >> arr[i];
+    insertion_sort(arr, n);
+    cout<< "Sorted array :" << endl;
+    for(int i=0; i<n; i++){
+        cout<< arr[i] << " ";
+    }
+}
