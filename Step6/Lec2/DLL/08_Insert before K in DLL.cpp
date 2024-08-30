9933b6184a0062255fb8ac4551aa555937aa6425
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-08-30 19:56:54 +0530

    Create 08_Insert before K in DLL.cpp

---
+++ Step6/Lec2/DLL/08_Insert before K in DLL.cpp
@@ -0,0 +1,78 @@
+  #include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+    
+    public:
+    Node(int data1,Node* next1, Node* back1){
+        data = data1;
+        next = next1;
+        back= back1;
+    }
+    
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+
+Node* constructDLL(vector<int> &arr){
+    if(arr.empty()) return nullptr;
+    
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void printLL(Node* head){
+    while(head != NULL){
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+Node* insertHead(Node* head, int val){
+    Node* temp = new Node(val);
+    temp->back = nullptr;
+    temp->next = head;
+    head->back = temp;
+    return temp;
+}
+
+Node* insertbeforeK(Node* head, int k, int val){
+    if(k==1){
+        return insertHead(head, val);
+    }
+    Node* temp = head;
+    int cnt = 0;
+    while(temp != NULL){
+        cnt++;
+        if(cnt == k)break;
+        temp = temp->next;
+    } 
+    Node* prev = temp->back;
+    Node* newNode = new Node(val, temp, prev);
+    prev -> next = newNode;
+    temp -> back = newNode;
+    return head;
+}
+
+int main(){
+    vector<int> arr = {12, 5, 8, 7};
+    Node* head = constructDLL(arr);
+    head = insertbeforeK(head,2,4);
+    printLL(head);
+    return 0;
+}
