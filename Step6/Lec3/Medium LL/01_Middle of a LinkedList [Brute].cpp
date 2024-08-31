c85bf9a74222bd20254f6bec97e15353475bbbbe
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-08-31 17:26:25 +0530

    Update and rename 01_Middle of a LinkedList [TortoiseHare Method].cpp to 01_Middle of a LinkedList [Brute].cpp

---
+++ Step6/Lec3/Medium LL/01_Middle of a LinkedList [Brute].cpp
@@ -0,0 +1,19 @@
+Node *findMiddle(Node *head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    Node* temp =head;
+    int cnt = 0;
+    while(temp != NULL){
+        cnt++;
+        temp = temp->next;
+    }
+    int middle = (cnt/2)+1;
+    temp = head;
+    while(temp != NULL){
+        middle = middle-1;
+        if(middle == 0) break;
+        temp = temp->next;
+    }
+    return temp;
+}
