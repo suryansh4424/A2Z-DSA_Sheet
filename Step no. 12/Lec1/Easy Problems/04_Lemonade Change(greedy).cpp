24ad0a3882186b8d5af6ba650c6996057d699383
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-09-03 12:38:04 +0530

    Create 04_Lemonade Change(greedy).cpp

---
+++ Step no. 12/Lec1/Easy Problems/04_Lemonade Change(greedy).cpp
@@ -0,0 +1,53 @@
+                            
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+bool lemonadeChange(vector<int>& bills) {
+    int five = 0;
+    int ten =0;
+    
+    for(int i=0; i<bills.size(); i++){
+        if(bills[i]==5){
+            five++;
+        }
+        else if(bills[i]==10){
+            if(five){
+                five--;
+                ten++;
+            }
+            else return false;
+        }
+        else{
+            if(ten && five){
+                five--;
+                ten--;
+            }
+            else if(five >= 3){
+                five -= 3;
+            }
+            else return false;
+        }
+    }
+    return true;
+}
+
+
+int main() {
+    vector<int> bills = {5, 5, 5, 10, 20};
+    cout << "Queues of customers: ";
+    for(int bill : bills){
+        cout << bill << " ";
+    }
+    cout << endl;
+    bool ans = lemonadeChange(bills);
+    if(ans)
+        cout << "It is possible to provide change for all customers." << endl;
+    else
+        cout << "It is not possible to provide change for all customers." << endl;
+    return 0;
+}
+                            
+                        
