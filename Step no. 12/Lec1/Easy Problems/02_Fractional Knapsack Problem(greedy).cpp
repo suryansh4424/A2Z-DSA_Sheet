6fac74220b2b7433592e580870243d8b76e0e0dc
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-09-03 13:13:39 +0530

    Update 02_Fractional Knapsack Problem(greedy).cpp

--- Step no. 12/Lec1/Easy Problems/02_Fractional Knapsack Problem(greedy).cpp
+++ Step no. 12/Lec1/Easy Problems/02_Fractional Knapsack Problem(greedy).cpp
@@ -40,3 +40,40 @@ int main() {
    cout << "The maximum value is " << setprecision(2) << fixed << ans;
    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+Link = ' https://www.naukri.com/code360/problems/fractional-knapsack_975286?leftPanelTabValue=DISCUSS '
+
+#include <bits/stdc++.h> 
+
+static bool comp(pair<int, int> a, pair<int, int> b) {
+    double r1 = (double) a.second / (double) a.first;
+    double r2 = (double) b.second / (double) b.first;
+    return r1 > r2;
+}
+
+double maximumValue(vector<pair<int, int>>& items, int n, int w) {
+    sort(items.begin(), items.end(), comp);
+    int currWeight = 0;
+    double finalValue = 0.0;
+    
+    for (int i = 0; i < n; i++) {
+        if (currWeight + items[i].first <= w) {
+            currWeight += items[i].first;
+            finalValue += items[i].second;
+        } else {
+            int remain = w - currWeight;
+            finalValue += (items[i].second / (double) items[i].first) * (double) remain;
+            break;
+        }
+    }
+    return finalValue;
+}
