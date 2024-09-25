0586cebbbada3c752bfd628d0dc34a94403aaab8
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-09-25 09:27:23 +0530

    Create 01_Preorder Traversal of Binary Tree.cpp

---
+++ 13/lec1/Traversals/01_Preorder Traversal of Binary Tree.cpp
@@ -0,0 +1,12 @@
+void pre(TreeNode<int> * root , vector<int>&ans){
+    if(root == NULL) return; 
+    ans.push_back(root->data);
+    pre(root->left , ans);
+    pre(root->right, ans);
+}
+
+vector<int> preOrder(TreeNode<int> * root){
+    vector<int> ans ;
+    pre(root , ans);
+    return ans ;
+}
